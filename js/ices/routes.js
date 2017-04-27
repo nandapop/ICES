c40c39170af65aee628cbed5939a5e99ec4f7808
fernanda <nandapop@gmail.com>
2017-04-27 08:49:24 +0200

    Added routes : about, motivation and goal in route.js

--- js/ices/routes.js
+++ js/ices/routes.js
@@ -10,7 +10,60 @@ angular.module('ices').config(function($urlRouterProvider, $stateProvider) {
             url: '/',
             templateUrl: "templates/main.html"
 //            controller: "",
-//            controllerAs: ""
+//            controllerAs: "",
+        })
+        .state("about", {
+            url:'/about',
+            templateUrl: "templates/about.html"
+//            controller: "",
+//            controllerAs: "",
+        })
+        .state("about.motivation", {
+            url:'/motivation',
+            templateUrl: "templates/about/motivation.html"
+//            controller: "",
+//            controllerAs: "",
+        })
+          .state("about.goal", {
+            url:'/goal',
+            templateUrl: "templates/about/goal.html"
+//            controller: "",
+//            controllerAs: "",
+        })
+        .state("studyIn", {
+            url:'/',
+            templateUrl: "templates/studyInSwiss.html"
+//            controller: "",
+//            controllerAs: "",
+        })
+        .state("icesCommunity", {
+            url:'/',
+            templateUrl: "template/icesCommunity.html"
+//            controller: "",
+//            controllerAs: "",
+        })
+        .state("events", {
+            url:'/',
+            templateUrl: "template/events.html"
+//            controller: "",
+//            controllerAs: "",
+        })
+        .state("news", {
+            url:'/',
+            templateUrl: "template/news.html"
+//            controller: "",
+//            controllerAs: "",
+        })
+        .state("photos", {
+            url:'/',
+            templateUrl: "template/photos.html"
+//            controller: "",
+//            controllerAs: "",
+        })
+        .state("contact", {
+            url:'/',
+            templateUrl: "template/contact.html"
+//            controller: "",
+//            controllerAs: "",
         });
-
 });
