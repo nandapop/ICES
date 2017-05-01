f2a4d031fafe9119dbd95b69680fc1a31ce37965
fernanda <nandapop@gmail.com>
2017-05-01 14:14:24 +0200

    Added routes and html files: ices community, events, news, photos and contact

--- js/ices/routes.js
+++ js/ices/routes.js
@@ -18,30 +18,72 @@ angular.module('ices').config(function($urlRouterProvider, $stateProvider) {
 //            controller: "",
 //            controllerAs: "",
         })
-        .state("about.motivation", {
+        .state('about.motivation', {
             url:'/motivation',
             templateUrl: "templates/about/motivation.html"
 //            controller: "",
 //            controllerAs: "",
         })
-          .state("about.goal", {
+          .state('about.goal', {
             url:'/goal',
             templateUrl: "templates/about/goal.html"
 //            controller: "",
 //            controllerAs: "",
         })
+        
+        .state('about.organization', {
+        url:'/organization',
+        templateUrl: "templates/about/organization.html"
+//            controller: "",
+//            controllerAs: "",
+        })
+
+        .state("about.directive", {
+        url:'/directive',
+        templateUrl: "templates/about/directive.html"
+//            controller: "",
+//            controllerAs: "",
+        })
+        
+        .state("about.previousDirective", {
+        url:'/previousDirective',
+        templateUrl: "templates/about/previousDirective.html"
+//            controller: "",
+//            controllerAs: "",
+        })
+        
         .state("studyIn", {
-            url:'/',
+            url:'/studyIn',
             templateUrl: "templates/studyInSwiss.html"
 //            controller: "",
 //            controllerAs: "",
         })
-        .state("icesCommunity", {
+
+        .state("icesCommunity.requirements", {
             url:'/',
-            templateUrl: "template/icesCommunity.html"
+            templateUrl: "template/icesCommunity/requirements.html"
 //            controller: "",
 //            controllerAs: "",
         })
+
+        .state("icesCommunity.benefit", {
+            url:'/',
+            templateUrl: "template/icesCommunity/benefit.html"
+//            controller: "",
+//            controllerAs: "",
+    })
+        .state("icesCommunity.membership", {
+            url:'/',
+            templateUrl: "template/icesCommunity/membership.html"
+//            controller: "",
+//            controllerAs: "",
+})
+        .state("icesCommunity.form", {
+            url:'/',
+            templateUrl: "template/icesCommunity/form.html"
+//            controller: "",
+//            controllerAs: "",
+})
         .state("events", {
             url:'/',
             templateUrl: "template/events.html"
